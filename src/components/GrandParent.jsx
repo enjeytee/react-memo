d40be32cc0daa7b44ceb5b48e9f100261b01e973
enjeytee <enjeytee@gmail.com>
2022-09-02 19:06:14 +0800

    af

---
+++ src/components/GrandParent.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+import Parent from "./Parent"
+
+function GrandParent() {
+    console.log("[👴🏼]   [ ]   [ ]   [ ] rendered")
+    return (
+        <div>
+            <p>I'm a GrandParent Component</p>
+            <Parent />
+            <Parent />
+        </div>
+    )
+}
+
+function areEqual(prevProps, nextProps) {
+    /*
+    return true if passing nextProps to render would return
+    the same result as passing prevProps to render,
+    otherwise return false
+    */    
+}
+
+export default React.memo(GrandParent, areEqual)
+
